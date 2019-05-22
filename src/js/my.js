e3b679aac3b2ce86306838f132a9cbc238dc90e4
Alex112 <oleksandr.mais@gmail.com>
2019-05-23 00:45:31 +0300

    fix scss structure

--- src/js/my.js
+++ src/js/my.js
@@ -4,20 +4,15 @@ $(document).ready(function(){
         arrows: false,
         autoplay: true,
     });
-  });
-
-  (function($) {
-    $(function() {
-      $("ul.tabs-caption").on("click", "li:not(.active)", function() {
-        $(this)
-          .addClass("active")
-          .siblings()
-          .removeClass("active")
-          .closest("div.tabs")
-          .find("div.tabs-content")
-          .removeClass("active")
-          .eq($(this).index())
-          .addClass("active");
-      });
+    $("ul.tabs-caption").on("click", "li:not(.active)", function() {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active")
+        .closest("div.tabs")
+        .find("div.tabs-content")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
     });
-  })(jQuery);
+  });
