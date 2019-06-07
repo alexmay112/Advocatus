0174096ebecbdf947be77496aa303dc8212311c7
Alex112 <oleksandr.mais@gmail.com>
2019-06-07 22:51:59 +0300

    add toTop button

--- src/js/my.js
+++ src/js/my.js
@@ -1,18 +1,24 @@
-$(document).ready(function(){
-    $('.slider-box').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
+$(document).ready(function () {
+    $('.slider-box').slick({dots: true, arrows: false, autoplay: true});
+    $("ul.tabs-caption").on("click", "li:not(.active)", function () {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active")
+            .closest("div.tabs")
+            .find("div.tabs-content")
+            .removeClass("active")
+            .eq($(this).index())
+            .addClass("active");
     });
-    $("ul.tabs-caption").on("click", "li:not(.active)", function() {
-      $(this)
-        .addClass("active")
-        .siblings()
-        .removeClass("active")
-        .closest("div.tabs")
-        .find("div.tabs-content")
-        .removeClass("active")
-        .eq($(this).index())
-        .addClass("active");
+
+    var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear'
+        };
+    $().UItoTop({
+        // easingType: 'easeOutQuart'
     });
-  });
+});
